7ede818dc5075f703e37a140c91acef575419caa
Yangjing <212719065@ge.com>
2019-10-29 17:03:18 +0800

    create a simple web pages use express

--- config.js
+++ config.js
@@ -5,8 +5,13 @@ import appRoot from 'app-root-path';
 
 export const IS_DEVELOPMENT = ['development', 'test'].includes(process.env.NODE_ENV);
 export const ROOT = IS_DEVELOPMENT ? path.join(__dirname, 'src') : path.join(appRoot.path, 'dist');
-
+export const port = process.env.PORT ? process.env.PORT : "8080";
+export const PUBLIC_PATH = path.join(__dirname, 'public');
+export const VIEW_PATH = path.join(PUBLIC_PATH, 'views');
 export default Object.freeze({
     IS_DEVELOPMENT,
-    appRoot
+    appRoot,
+    port,
+    PUBLIC_PATH,
+    VIEW_PATH
 });
--- server.js
+++ server.js
@@ -1,3 +1,53 @@
-import logger from './logger';
+"use strict";
+import express from 'express';
+import path from 'path';
+import hbs from 'hbs';
 
-logger.info("Start Server...");
+import logger from './logger';
+import config from './config';
+
+const port = config.port;
+
+var app = express();
+
+app.set('views', config.VIEW_PATH);
+app.set('view engine', 'hbs');
+app.use(express.static(config.VIEW_PATH)); // app.use() is for registering some middlewares, you can customize it
+
+hbs.registerPartials(path.join(config.VIEW_PATH, 'partials'))
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: "Home Page"
+    })
+});
+
+app.get('/user', (req, res) => {
+    res.render('user.hbs', {
+        pageTitle: "User Page",
+        userInfo: JSON.stringify({
+            name: 'Username',
+            likes: ['reading', 'singing', 'skating']
+        }, undefined, 2)
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help.hbs', {
+        pageTitle: 'Help Page'
+    })
+})
+
+app.get('/error', (req, res) => {
+    res.render('error.hbs', {
+        pageTitle: 'Help Page',
+        errorMessage: 'Unable to connect this page.'
+    })
+})
+
+app.listen(port, () => {
+    logger.info(`Application started on  ${port}`);
+});
